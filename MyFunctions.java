2a74026cf663a498ecddff7a71dc67351ad04eff
MaherSaleem <the-maher@live.com>
2015-12-24 14:17:39 +0200

    MyFuntion class
    عشان الفنكشنات العامة اللي ملهاش دخل بالداتا بيز

---
+++ MyFunctions.java
@@ -0,0 +1,14 @@
+
+public class MyFunctions {
+	
+	
+	/*
+	 * to be used when we read a text from textbox
+	 */
+	static Integer getRealValueInt(String s){
+		if (s == null) return null;
+		s=s.trim();
+		if(s.isEmpty()) return null;//"" string (to add it as NULL in datebase)
+		return Integer.parseInt(s);	
+	}
+}
